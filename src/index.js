6a54687cf3927eeac01a9887a7c50b2626eb73ec
ohoroyoi <soongan.jeon@gmail.com>
2020-10-08 07:12:40 +0900

    배포일 지정

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 ReactDOM.render(
   <React.StrictMode>
     <div>도넛을 위한 지도</div>
+    <div>until 10.31</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
